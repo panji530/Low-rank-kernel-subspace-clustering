5c20326254b1fdfd3929418cbe98b38ae356e646
Pan Ji <pan.ji@adelaide.edu.au>
2017-09-15 18:43:45 +0930

    Update polynKernelMatrix.m

--- polynKernelMatrix.m
+++ polynKernelMatrix.m
@@ -1,4 +1,5 @@
 function K = polynKernelMatrix(X,c,d)
+% Author: Pan Ji, University of Adelaide
 if(nargin<3)
 	d = 2;
 end
@@ -8,15 +9,12 @@ end
 
 if(d == -2)
 	tmp = X'*X;
-	K   = double(tmp>0.9)+double(tmp<-0.9);
-	%K   = K/trace(K);
+	K   = double(tmp>0.9)+double(tmp<-0.9);	
 elseif(d == -1)
 	tmp = X'*X;
-	K   = double(tmp>0.9)-double(tmp<-0.9);
-	%K   = K/trace(K);
+	K   = double(tmp>0.9)-double(tmp<-0.9);	
 else
-	K = (X'*X+c).^d;
-	%K = K/trace(K);
+	K = (X'*X+c).^d;	
 end
 
 end
